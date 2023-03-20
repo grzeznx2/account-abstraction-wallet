ba4bf5c69b1275f5b5be06086f9f717cfd756097
grzeznx2 <grzeznx2@gmail.com>
2023-03-20 21:23:57 +0100

    Create EntryPoint interface

---
+++ src/interfaces/IEntryPoint.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "./UserOperation.sol";
+import "./IAggregator.sol";
+
+interface IEntryPoint {
+
+    struct UserOpsPerAggregator {
+        IAggregator aggregator;
+        UserOperation[] ops;
+        bytes signature;
+    }
+
+    struct ReturnInfo {
+        uint256 preOpGas;
+        uint256 prefund;
+        bool sigFailed;
+        uint48 validAfter;
+        uint48 validUntil;
+        bytes paymasterContext;
+    }
+
+    struct StakeInfo {
+        uint256 stake;
+        uint256 unstakeDelaySec;
+    }
+
+    struct AggregatorStakeInfo {
+        address actualAggregator;
+        StakeInfo stakeInfo;
+    }
+
+    error ValidationResult(ReturnInfo returnInfo, StakeInfo senderInfo, StakeInfo factoryInfo, StakeInfo paymasterInfo);
+
+    error ValidationResultWithAggregation(ReturnInfo returnInfo, StakeInfo senderInfo, StakeInfo factoryInfo, StakeInfo paymasterInfo, AggregatorStakeInfo aggregatorInfo);
+
+    function handleOps(UserOperation[] calldata ops, address payable beneficiary) external;
+
+    function handleAggregatedOps(UserOpsPerAggregator[] calldata opsPerAggregator, address payable beneficiary) external;
+
+    function simulateValidation(UserOperation calldata op) external;
+
+
+}
