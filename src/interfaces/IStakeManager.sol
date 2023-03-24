afc7554191b0adeb2664a678c5d17d225f48535d
grzeznx2 <grzeznx2@gmail.com>
2023-03-24 16:19:28 +0100

    Finish stake manager interface

--- src/interfaces/IStakeManager.sol
+++ src/interfaces/IStakeManager.sol
@@ -22,11 +22,38 @@ interface IStakeManager {
     event StakeUnlocked (
         address indexed account,
         uint256 withdrawTime
-    )
+    );
 
     event StakeWithdrawn (
         address indexed account,
         address withdrawAddress,
         uint256 amount
-    )
+    );
+
+    struct DepositInfo {
+        uint112 deposit;
+        bool staked;
+        uint112 stake;
+        uint32 unstakeDelaySec;
+        uint48 withdrawTime;
+    }
+
+    struct StakeInfo {
+        uint256 stake;
+        uint256 unstakeDelaySec;
+    }
+
+    function getDepositInfo(address account) external view returns(DepositInfo memory info);
+
+    function balanceOf(address account) external view returns(uint256);
+
+    function depositTo(address account) external payable;
+
+    function addStake(uint32 unstakeDelaySec) external payable;
+
+    function unlockStake() external;
+
+    function withdrawStake(address payable withdrawAddress) external;
+
+    function withdrawTo(address payable withdrawAddress, uint256 withdrawAmount) external;
 }
