fd5a386d536deddf08590ea9883460cc17b22f68
grzeznx2 <grzeznx2@gmail.com>
2023-03-27 21:59:45 +0200

    Implement validateUserOp method

--- src/core/BaseAccount.sol
+++ src/core/BaseAccount.sol
@@ -13,4 +13,25 @@ abstract contract BaseAccount is IAccount {
     function _validateSignature(UserOperation calldata userOp, bytes32 userOpHash) internal virtual returns (uint256 validationData);
 
     function _validateAndUpdateNonce(UserOperation calldata userOp) internal virtual;
+
+    function validateUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external override virtual returns(uint256 validationData){
+            _requireFromEntryPoint();
+            validationData = _validateSignature(userOp, userOpHash);
+            if(userOp.initCode.length == 0){
+                _validateAndUpdateNonce(userOp);
+            }
+            _payPrefund(missingAccountFunds);
+        }
+
+    function _requireFromEntryPoint() internal virtual view {
+        require(msg.sender == address(entryPoint()), "account: not from EntryPoint");
+    }
+
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if(missingAccountFunds != 0){
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
+            (success);
+        }
+    }
 }
